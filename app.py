921e7bd9db3d3b07a1cf7b1cfe7f7b8ac210a4d2
hweng1 <89861149+hweng1@users.noreply.github.com>
2021-12-07 18:35:14 -0500

    1

--- app.py
+++ app.py
@@ -1,39 +1,43 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from calculations import find_user_id, profit_loss, time, BB_per_hr, pl_per_hr, user_table, new_session_id
 import sqlite3
-import os
 # import sqlalchemy
 # from logincheck import loginid
+    
 
 app = Flask(__name__)
+app.secret_key = 'secretkeyfortexasholdemamazingapp'
 
-conn = sqlite3.connect('Profile.db')
+conn = sqlite3.connect('Profile.db', check_same_thread=False)
 my_cursor = conn.cursor()
 
-@app.route('/')
-def homepage():
-    return render_template('login_page.html')
 
 @app.route('/', methods=['GET','POST'])
 def checklogin():
     if request.method == 'POST':
+        session.pop('user_name', None)
+        session.pop('user_password', None)
         login_id = (request.form['login_id'])
         password = (request.form['password'])
-        query1 = 'SELECT Login_id, Password From Profile WHERE Login_id = {un} AND Password = {pw}'.format(un = login_id, pw = password )
+        query1 = "SELECT Login_id, Password From Profile WHERE Login_id = '{un}'AND Password = '{pw}'".format(un = login_id, pw = password )
         rows = my_cursor.execute(query1)
         rows = rows.fetchall()
         if len(rows) == 1:
+            session['user_name'] = login_id
+            session['password'] = password
             return render_template('result_output.html'
             )
         else:
-            return redirect('/register')
+            return redirect('/')
+    else:
+        return render_template('login_page.html')
 
 @app.route('/register', methods = ['GET', 'POST'])
 def registerpage():
     if request.method == 'POST':
         D_login_id = request.form['D_login_id']
         D_password = request.form['D_password']
-        query2 = 'INSERT INTO Profile (Login_id, Password) VALUES ({u} ,{p})'.format(u = D_login_id, p = D_password)
+        query2 = "INSERT INTO Profile (Login_id, Password) VALUES ('{u}','{p}')".format(u = D_login_id, p = D_password)
         my_cursor.execute(query2)
         conn.commit()
         return redirect('/')
@@ -54,28 +58,28 @@ def index():
         session_id = new_session_id(Login_id=request.form['login_id'])
         cur = sqlite3.connect('Profile.db').cursor()
         cur.execute('INSERT INTO Session (BB, SB, win, loss, location, hours) VALUES(?,?,?,?,?,?,?)',(session_id, big_blind, small_blind, win, loss, location, hours))
-        conn.connection.commit()
+        conn.commit()
         cur.close()
         return 'data has been entered'
     return render_template('data_input.html')
 
-# @app.route('/result')
-# def result():
-#     return render_template('result_output.html')
+@app.route('/result')
+def result():
+    return render_template('result_output.html')
 
-# @app.route('/delete', method=['GET','POST'])
-# def delete():
-#         if request.method == 'POST':
-#             try:
-#                 dl_id = (request.form['dl_id'])
-#                 query1 = 'DELETE * From Session WHERE session_id = {un}'.format(un = dl_id)
-#                 my_cursor.execute(query1)
-#                 conn.commit()
-#                 return render_template('success.html', session_id = dl_id)
-#             except:
-#                 return render_template('delete.html')
-#         else:
-#             return render_template('delete.html')
+@app.route('/delete', methods=['GET','POST'])
+def delete():
+    if request.method == 'POST':
+        try:
+            dl_id = (request.form['dl_id'])
+            query1 = 'DELETE * From Session WHERE session_id = {un}'.format(un = dl_id)
+            my_cursor.execute(query1)
+            conn.commit()
+            return render_template('success.html', session_id = dl_id)
+        except:
+            return render_template('delete.html')
+    else:
+        return render_template('delete.html')
 
 if __name__ =='__main__':
     app.run(debug=True)
